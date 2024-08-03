0ba5316140230d3af039eb9272e75935f9a07f39
rkedzies <rob.kedzior94@gmail.com>
2024-08-03 14:51:02 +0200

    Initial commit with all fiels

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const Home = (props: Props) => {
+    return (
+        <section className='home'><div className='home-content'>
+            <header className='home-header'>
+                Welcome, Robert
+            </header>
+        </div></section>
+    )
+}
+
+export default Home
